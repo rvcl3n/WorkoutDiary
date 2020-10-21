63d59045cfc5bcf43948fdeb39e084c04357deff
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-21 19:43:17 +0300

    Added a new component

--- WorkoutDiaryFrontEnd/src/app/app-routing.module.ts
+++ WorkoutDiaryFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'workoutlist', component: WorkoutListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WorkoutDiaryFrontEnd/src/app/app.module.ts
+++ WorkoutDiaryFrontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkoutListComponent
   ],
   imports: [
     BrowserModule,
