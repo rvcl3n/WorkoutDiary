af4421f956bde03aab97e685e5037844d93b89af
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-21 22:15:30 +0300

    Added a Home Component

--- WorkoutDiaryFrontEnd/src/app/app.module.ts
+++ WorkoutDiaryFrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkoutListComponent } from './workout-list/workout-list.component';
 import { WorkoutComponent } from './workout/workout.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkoutListComponent,
-    WorkoutComponent
+    WorkoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
