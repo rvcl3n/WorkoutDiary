f4c66f42b53a5312188399b4d78acba8c8fa9752
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-22 19:37:17 +0300

    Fixed nav styles

--- WorkoutDiaryFrontEnd/src/app/app.module.ts
+++ WorkoutDiaryFrontEnd/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MenuComponent } from './menu/menu.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
