fb6b7e9eb6ca4192d8f5b5f0771aa549f0915f7c
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-27 23:33:14 +0200

    Added a workout-card component

--- WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
+++ WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkoutListComponent } from './workout-list/workout-list.component';
+import { WorkoutCardComponent } from './workout-card/workout-card.component';
 
 
 
 @NgModule({
-  declarations: [WorkoutListComponent],
+  declarations: [WorkoutListComponent, WorkoutCardComponent],
   imports: [
     CommonModule
   ]
