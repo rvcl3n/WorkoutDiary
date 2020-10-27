df1f792b8d2627ad9e3dbd3f10d2ba99f65b5dff
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-27 23:26:22 +0200

    Added a new component for the workout module

--- WorkoutDiaryFrontEnd/src/app/app-routing.module.ts
+++ WorkoutDiaryFrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { WorkoutListComponent } from './workout-list/workout-list.component';
+import { WorkoutListComponent } from './workout/workout-list/workout-list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'workoutlist', component: WorkoutListComponent},
+  { path: 'workoutlist', component: WorkoutListComponent} //I guess this approach doesn't support lazy loading
 ];
 
 @NgModule({
--- WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
+++ WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WorkoutListComponent],
   imports: [
     CommonModule
   ]
