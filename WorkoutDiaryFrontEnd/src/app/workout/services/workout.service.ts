9165f3989102a7bcab13b9c20776a0496bfa17db
rvcl3n <d.rudenko.1996@gmail.com>
2020-11-03 15:14:14 +0200

    Added a service for workouts

--- WorkoutDiaryFrontEnd/src/app/workout/services/workout.service.ts
+++ WorkoutDiaryFrontEnd/src/app/workout/services/workout.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Workout } from 'src/app/_models/workout.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWorkouts(): Observable<Array<Workout>> {
+    return this.http.get<Array<Workout>>('http://localhost:3000/workouts');
+  }
 }
