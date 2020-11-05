289736b68e0db28fbfd3a2249f57a401bc413f8c
rvcl3n <d.rudenko.1996@gmail.com>
2020-11-05 23:27:16 +0200

    Added an environment module to the workout service

--- WorkoutDiaryFrontEnd/src/app/workout/services/workout.service.ts
+++ WorkoutDiaryFrontEnd/src/app/workout/services/workout.service.ts
@@ -3,23 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Workout } from 'src/app/_models/workout.model';
 import { WorkoutExercise } from 'src/app/_models/workoutExercise.model';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutService {
 
+  private serverUrl = environment.urlAddress;
+
   constructor(private http: HttpClient) { }
 
   getWorkouts(): Observable<Array<Workout>> {
-    return this.http.get<Array<Workout>>('http://localhost:3000/workouts');
+    return this.http.get<Array<Workout>>(`${this.serverUrl}/workouts`);
   }
 
   getWorkoutExercises(): Observable<Array<WorkoutExercise>> {
-    return this.http.get<Array<WorkoutExercise>>('http://localhost:3000/workoutexercises');
+    return this.http.get<Array<WorkoutExercise>>(`${this.serverUrl}/workoutexercises`);
   }
 
   addWorkoutExercise(exercise: WorkoutExercise): Observable<WorkoutExercise> {
-    return this.http.post<WorkoutExercise>('http://localhost:3000/workoutexercises', exercise);
+    return this.http.post<WorkoutExercise>(`${this.serverUrl}/workoutexercises`, exercise);
   }
 }
