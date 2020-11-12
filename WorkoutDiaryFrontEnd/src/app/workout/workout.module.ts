17424f1f2539379defac2c98c736ea073f71447b
rvcl3n <d.rudenko.1996@gmail.com>
2020-11-12 23:50:48 +0200

    Added a mat button to the modal

--- WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
+++ WorkoutDiaryFrontEnd/src/app/workout/workout.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -33,7 +34,8 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatDialogModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatButtonModule
   ],
   exports: [
     WorkoutListComponent,
